fd3f26747ebf39c653f80cf6ef04127624129e16
Saeed-Iqtaish <saeed.ag2004@gmail.com>
2025-04-27 21:22:44 +0300

    made form add contacts to a list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ContactForm from './components/ContactForm';
 
 function App() {
 
+  const [contact,setContact] = useState([]);
+  
+  const addContact = (newContact) => {
+    setContact([...contact,newContact])
+  }
+
+
+  //to check if contacts are being added or not
+  useEffect(() => {
+    console.log(contact);
+  }, [contact]);
+  
+
   return (
     <>
-    <ContactForm></ContactForm>
+    <ContactForm addContact = {addContact}/>
     </>
   );
 }
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,24 +1,42 @@
 import React from 'react'
 import { useState } from 'react'
 
-function ContactForm() {
+function ContactForm(props) {
 
 
     const [userName, setUserName] = useState("");
     const [email, setEmail] = useState("");
 
+    const addContact = (e) => {
+        e.preventDefault();
+
+        let contact = {
+            userName,
+            email
+        };
+
+        props.addContact(contact);
+
+        setUserName("");
+        setEmail("");
+    }
+
     return (
         <>
             <h2>Add a Contact</h2>
-            <form>
+            <form onSubmit={addContact}>
                 <input
                     type="text"
                     placeholder='Name'
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
                 />
 
                 <input
                     type="text"
                     placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <button type="submit">
